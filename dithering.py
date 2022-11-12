1c22a956043d985a89bb3d0ff0fe61d573301a57
ckonst <christian.konstantinov98@gmail.com>
2022-11-12 16:05:28 -0500

    add transpose matrices

--- dithering.py
+++ dithering.py
@@ -8,6 +8,7 @@ Created on Fri Feb 25 15:12:44 2022
 # TODO: add alpha channel support ?
 # TODO: Serpentine diffusion ?
 
+from functools import wraps
 from enum import Enum
 from typing import Tuple
 
@@ -20,14 +21,14 @@ class DiffusionMatrix(Enum):
     """Matrices/Kernels for Error Diffusion
 
     Args:
-        Enum (Tuple[Tuple[float]]): Tuple of (x indices, y indices, coefficients)
+        Enum (Tuple[Tuple[Number]]): Tuple of (x indices, y indices, coefficients)
     """
 
     ATKINSON = (
         (1, 2, -1, 0, 1, 0),
         (0, 0,  1, 1, 1, 2),
         (1 / 8, 1 / 8, 1 / 8, 1 / 8, 1 / 8, 1 / 8)
-    ),
+    )
     BURKES = (
         (1, 2, -2, -1, 0, 1, 2),
         (0, 0,  1,  1, 1, 1, 1),
@@ -37,40 +38,52 @@ class DiffusionMatrix(Enum):
         (1, -1, 0, 1),
         (0,  1, 1, 1),
         (7 / 16, 3 / 16, 5 / 16, 1 / 16)
-    ),
+    )
     FALSE_FLOYD_STEINBERG = (
         (1, 0, 1),
         (0, 1, 1),
         (3 / 8, 3 / 8, 2 / 8)
-    ),
+    )
     JARVIS_JUDICE_NINKE = (
         (1, 2, -2, -1, 0, 1, 2, -2, -1, 0, 1, 2),
         (0, 0,  1,  1, 1, 1, 1,  2,  2, 2, 2, 2),
         (7 / 48, 5 / 48, 3 / 48, 5 / 48, 7 / 48, 5 / 48,
          3 / 48, 1 / 48, 3 / 48, 5 / 48, 3 / 48, 1 / 48)
-    ),
+    )
     STUCKI = (
         (1, 2, -2, -1, 0, 1, 2, -2, -1, 0, 1, 2),
         (0, 0,  1,  1, 1, 1, 1,  2,  2, 2, 2, 2),
         (8 / 48, 4 / 48, 2 / 48, 4 / 48, 8 / 48, 4 / 48,
          2 / 48, 1 / 48, 2 / 48, 4 / 48, 2 / 48, 1 / 48)
-    ),
+    )
     SIERRA = (
         (1, 2, -2, -1, 0, 1, 2, -1, 0, 1),
         (0, 0,  1,  1, 1, 1, 1,  2, 2, 2),
         (5 / 32, 3 / 32, 2 / 32, 4 / 32, 5 / 32,
          4 / 32, 2 / 32, 2 / 32, 3 / 32, 2 / 32)
-    ),
+    )
     TWO_ROW_SIERRA = (
         (1, 2, -2, -1, 0, 1, 2),
         (0, 0,  1,  1, 1, 1, 1),
         (4 / 16, 3 / 16, 1 / 16, 2 / 16, 3 / 16, 2 / 16, 1 / 16)
-    ),
+    )
     SIERRA_LITE = (
         (1, -1, 0),
         (0,  1,  1),
         (2 / 4, 1 / 4, 1 / 4)
     )
+
+
+def transpose(xs, ys, matrix):
+    return ys, xs, matrix
+
+
+# Set a field that points to the Transpose Matrices
+setattr(
+    DiffusionMatrix, 'T', Enum('TransposeDiffusionMatrix', {
+        m.name: transpose(*m.value) for m in DiffusionMatrix
+    })
+)
 
 
 def _quantize_uniform(image, bit_depth):
@@ -82,7 +95,7 @@ def floyd_steinberg(image, bit_depth):
     return dither(image, bit_depth, *DiffusionMatrix.FLOYD_STEINBERG.value)
 
 
-@njit
+@ njit
 def dither(image, bit_depth, matrix):
     height, width, channels = image.shape
     palette_size = (1 << bit_depth) - 1
@@ -107,6 +120,7 @@ def _verify(image, bit_depth, num_channels=3) -> bool:
         palette_size *= np.unique(image[:, :, i]).shape[0]
     expected_palette_size = (1 << bit_depth) ** num_channels
     return palette_size <= expected_palette_size
+
 
 # TODO: Move to another file
 if __name__ == '__main__':
