ae7a97142965010100bef9af77d4ac7bbb65e5c3
ckonst <christian.konstantinov98@gmail.com>
2022-11-26 01:33:13 -0500

    add green noise halftoning

--- dithering.py
+++ dithering.py
@@ -8,9 +8,11 @@ Created on Fri Feb 25 15:12:44 2022
 # TODO: Add alpha channel support
 # TODO: Add serpentine diffusion
 # TODO: Add Gamma Correction
+# TODO: Add filter preturbation
 
 from functools import lru_cache
 from enum import Enum
+from typing import Tuple
 
 import numpy as np
 from numba import njit, cfunc
@@ -25,86 +27,86 @@ class DiffusionMatrix(Enum):
     """
 
     ATKINSON = (
-        (1, 0, 0.125),
-        (2, 0, 0.125),
-        (-1, 1, 0.125),
-        (0, 1, 0.125),
-        (1, 1, 0.125),
-        (0, 2, 0.125)
+        (1, 0, 1 / 8),
+        (2, 0, 1 / 8),
+        (-1, 1, 1 / 8),
+        (0, 1, 1 / 8),
+        (1, 1, 1 / 8),
+        (0, 2, 1 / 8)
     )
     BURKES = (
-        (1, 0, 0.25),
-        (2, 0, 0.125),
-        (-2, 1, 0.0625),
-        (-1, 1, 0.125),
-        (0, 1, 0.25),
-        (1, 1, 0.125),
-        (2, 1, 0.0625)
+        (1, 0, 8 / 32),
+        (2, 0, 4 / 32),
+        (-2, 1, 2 / 32),
+        (-1, 1, 4 / 32),
+        (0, 1, 8 / 32),
+        (1, 1, 4 / 32),
+        (2, 1, 2 / 32)
     )
     FLOYD_STEINBERG = (
-        (1, 0, 0.4375),
-        (-1, 1, 0.1875),
-        (0, 1, 0.3125),
-        (1, 1, 0.0625)
+        (1, 0, 7 / 16),
+        (-1, 1, 3 / 16),
+        (0, 1, 5 / 16),
+        (1, 1, 1 / 16)
     )
     FALSE_FLOYD_STEINBERG = (
-        (1, 0, 0.375),
-        (0, 1, 0.375),
-        (1, 1, 0.25)
+        (1, 0, 3 / 8),
+        (0, 1, 3 / 8),
+        (1, 1, 2 / 8)
     )
     JARVIS_JUDICE_NINKE = (
-        (1, 0, 0.14583333333333334),
-        (2, 0, 0.10416666666666667),
-        (-2, 1, 0.0625),
-        (-1, 1, 0.10416666666666667),
-        (0, 1, 0.14583333333333334),
-        (1, 1, 0.10416666666666667),
-        (2, 1, 0.0625),
-        (-2, 2, 0.020833333333333332),
-        (-1, 2, 0.0625),
-        (0, 2, 0.10416666666666667),
-        (1, 2, 0.0625),
-        (2, 2, 0.020833333333333332)
+        (1, 0, 7 / 48),
+        (2, 0, 5 / 48),
+        (-2, 1, 3 / 48),
+        (-1, 1, 5 / 48),
+        (0, 1, 7 / 48),
+        (1, 1, 5 / 48),
+        (2, 1, 3 / 48),
+        (-2, 2, 1 / 48),
+        (-1, 2, 3 / 48),
+        (0, 2, 5 / 48),
+        (1, 2, 3 / 48),
+        (2, 2, 1 / 48)
     )
     STUCKI = (
-        (1, 0, 0.16666666666666666),
-        (2, 0, 0.08333333333333333),
-        (-2, 1, 0.041666666666666664),
-        (-1, 1, 0.08333333333333333),
-        (0, 1, 0.16666666666666666),
-        (1, 1, 0.08333333333333333),
-        (2, 1, 0.041666666666666664),
-        (-2, 2, 0.020833333333333332),
-        (-1, 2, 0.041666666666666664),
-        (0, 2, 0.08333333333333333),
-        (1, 2, 0.041666666666666664),
-        (2, 2, 0.020833333333333332)
+        (1, 0, 8 / 48),
+        (2, 0, 4 / 48),
+        (-2, 1, 2 / 48),
+        (-1, 1, 4 / 48),
+        (0, 1, 8 / 48),
+        (1, 1, 4 / 48),
+        (2, 1, 2 / 48),
+        (-2, 2, 1 / 48),
+        (-1, 2, 2 / 48),
+        (0, 2, 4 / 48),
+        (1, 2, 2 / 48),
+        (2, 2, 1 / 48)
     )
     SIERRA = (
-        (1, 0, 0.15625),
-        (2, 0, 0.09375),
-        (-2, 1, 0.0625),
-        (-1, 1, 0.125),
-        (0, 1, 0.15625),
-        (1, 1, 0.125),
-        (2, 1, 0.0625),
-        (-1, 2, 0.0625),
-        (0, 2, 0.09375),
-        (1, 2, 0.0625)
+        (1, 0, 5 / 32),
+        (2, 0, 3 / 32),
+        (-2, 1, 2 / 32),
+        (-1, 1, 4 / 32),
+        (0, 1, 5 / 32),
+        (1, 1, 4 / 32),
+        (2, 1, 2 / 32),
+        (-1, 2, 2 / 32),
+        (0, 2, 3 / 32),
+        (1, 2, 2 / 32)
     )
     TWO_ROW_SIERRA = (
-        (1, 0, 0.25),
-        (2, 0, 0.1875),
-        (-2, 1, 0.0625),
-        (-1, 1, 0.125),
-        (0, 1, 0.1875),
-        (1, 1, 0.125),
-        (2, 1, 0.0625)
+        (1, 0, 4 / 16),
+        (2, 0, 3 / 16),
+        (-2, 1, 1 / 16),
+        (-1, 1, 2 / 16),
+        (0, 1, 3 / 16),
+        (1, 1, 2 / 16),
+        (2, 1, 1 / 16)
     )
     SIERRA_LITE = (
-        (1, 0, 0.5),
-        (-1, 1, 0.25),
-        (0, 1, 0.25)
+        (1, 0, 2 / 4),
+        (-1, 1, 1 / 4),
+        (0, 1, 1 / 4)
     )
 
     @property
@@ -114,7 +116,25 @@ class DiffusionMatrix(Enum):
         Return the matrix rotated 180 degrees for hysteresis filter preturbation.
         Calculate the matrix once retrieve from the cache on subsequent calls.
         """
-        return tuple((-i, -j, c) for i, j, c in self.value)
+        return tuple((-x, -y, c) for x, y, c in self.value)
+
+    @property
+    @lru_cache(maxsize=1)
+    def hysteresis_reversed(self):
+        """
+        Return the matrix rotated 180 degrees for hysteresis filter preturbation.
+        Calculate the matrix once retrieve from the cache on subsequent calls.
+        """
+        return tuple((x, -y, c) for x, y, c in self.value)
+
+    @property
+    @lru_cache(maxsize=1)
+    def reversed(self):
+        """
+        Return the matrix flipped horizontally for odd-row serpentine diffusion.
+        Calculate the matrix once retrieve from the cache on subsequent calls.
+        """
+        return tuple((-x, y, c) for x, y, c in self.value)
 
 
 def atkinson(image, bit_depth):
@@ -153,13 +173,30 @@ def sierra_lite(image, bit_depth):
     return dither(image, bit_depth, DiffusionMatrix.SIERRA_LITE.value)
 
 
+def lau_arce_gallagher(image, bit_depth, hysteresis_constant: float = 1, serpentine: bool = True):
+    return green_noise_halftone(
+        image, bit_depth, (DiffusionMatrix.FLOYD_STEINBERG.hysteresis, DiffusionMatrix.FLOYD_STEINBERG.hysteresis_reversed,
+                           DiffusionMatrix.STUCKI.value, DiffusionMatrix.STUCKI.reversed), hysteresis_constant, serpentine
+    )
+
+
+@cfunc('boolean(int64, int64, int64, int64)')
+def check_bounds(x, y, width, height):
+    return 0 <= x < width and 0 <= y < height
+
+
+@cfunc('float64(float64)')
+def clamp(color):
+    return max(0, min(color, 255))
+
+
 @cfunc('float64(float64, int64)')
 def quantize_uniform(color: float, palette_size: float) -> int:
     return round(color / 255.0 * palette_size) / palette_size * 255.0
 
 
 @njit
-def dither(image: np.ndarray, bit_depth: int, matrix: DiffusionMatrix) -> np.ndarray:
+def dither(image: np.ndarray, bit_depth: int, matrix: Tuple) -> np.ndarray:
     height, width, channels = image.shape
     palette_size = (1 << bit_depth) - 1
     for y in range(height):
@@ -170,6 +207,41 @@ def dither(image: np.ndarray, bit_depth: int, matrix: DiffusionMatrix) -> np.nda
                 image[y, x, c] = quant_color
                 quant_error = color - quant_color
                 for u, v, k in matrix:
-                    if 0 <= x + u < width and 0 <= y + v < height:
+                    if check_bounds(x + u, y + v, width, height):
                         image[y + v, x + u, c] += quant_error * k
+    return image
+
+
+@njit
+def green_noise_halftone(image: np.ndarray, bit_depth: int, matrix: Tuple, H: int, serpentine: bool = True) -> np.ndarray:
+    height, width, channels = image.shape
+    palette_size = (1 << bit_depth) - 1
+
+    if serpentine:
+        hysteresis, hysteresis_reversed, diffusion, diffusion_reversed = matrix
+        hysteresis_matrices = hysteresis, hysteresis_reversed
+        diffusion_matrices = diffusion, diffusion_reversed
+    else:
+        hysteresis, _, diffusion, _ = matrix
+        hysteresis_matrices = hysteresis, hysteresis
+        diffusion_matrices = diffusion, diffusion
+
+    for y in range(height):
+        aim = y % 2
+        range_args = ((0, width, 1), (width, 0, -1))
+        for x in range(*range_args[aim]):
+            for c in range(channels):
+                input_color = image[y, x, c]
+                for u, v, k in hysteresis_matrices[aim]:
+                    if check_bounds(x + u, y + v, width, height):
+                        image[y, x, c] += H * k * image[y + v, x + u, c]
+                color = image[y, x, c]
+                quant_color = quantize_uniform(color, palette_size)
+                image[y, x, c] = quant_color
+                quant_error = quant_color - input_color
+                for u, v, k in diffusion_matrices[aim]:
+                    if check_bounds(x + u, y + v, width, height):
+                        image[y + v, x + u, c] -= quant_error * k
+                image[y, x, c] = clamp(image[y, x, c])
+
     return image
--- test/test_dithering.py
+++ test/test_dithering.py
@@ -4,10 +4,11 @@ import numpy as np
 from PIL import Image
 
 from dithering import (
-    DiffusionMatrix, 
-    floyd_steinberg, 
+    DiffusionMatrix,
+    floyd_steinberg,
     jarvis_judice_ninke,
-    sierra
+    sierra,
+    lau_arce_gallagher
 )
 
 
@@ -28,25 +29,31 @@ class TestDithering(unittest.TestCase):
         image.save(output_path)
         print(f'Saved image with {output_path = }')
 
-    def get_dithered_image(self, image, bit_depth, func):
+    def get_dithered_image(self, image, bit_depth, func, **kwargs):
         input_data = np.array(image).astype(np.float32)
-        output_data = func(input_data, bit_depth).astype(np.uint8)
+        output_data = func(input_data, bit_depth, **kwargs).astype(np.uint8)
         return output_data
 
     def verify_palette_size(self, image, bit_depth, num_channels=3) -> bool:
         palette_size = 1
         for i in range(num_channels):
+            print(np.unique(image[:, :, i]))
+            print(np.unique(image[:, :, i]).size)
             palette_size *= np.unique(image[:, :, i]).size
         expected_palette_size = (1 << bit_depth) ** num_channels
+        print(expected_palette_size, palette_size)
         return palette_size <= expected_palette_size
 
     def test_real_palette_size(self):
         input_path = './images/hatsune_miku.png'
-        output_path = './images/hatsune_miku_dithered_2_bits.png'
         image = self.load_image(input_path)
         bit_depth = 2
         output_data = self.get_dithered_image(
             image, bit_depth, floyd_steinberg)
+        self.assertTrue(self.verify_palette_size(output_data, bit_depth))
+
+        output_data = self.get_dithered_image(
+            image, bit_depth, lau_arce_gallagher,  hysteresis_constant=1/2.5)
         self.assertTrue(self.verify_palette_size(output_data, bit_depth))
 
     def test_visual(self):
@@ -81,7 +88,23 @@ class TestDithering(unittest.TestCase):
             (0, -1, 0.3125),
             (-1, -1, 0.0625)
         )
-        self.assertEquals(DiffusionMatrix.FLOYD_STEINBERG.hysteresis, FLOYD_STEINBERG_HYSTERESIS)
+        self.assertEqual(
+            DiffusionMatrix.FLOYD_STEINBERG.hysteresis, FLOYD_STEINBERG_HYSTERESIS)
+
+    def test_green_noise_halftone(self):
+        input_path = './images/smile.png'
+        output_path = './images/smile_green_noise_dithered_1_bit.png'
+        image = self.load_image(input_path)
+        dithered_image = self.get_dithered_image(
+            image, 1, lau_arce_gallagher, hysteresis_constant=1/2.5)
+        self.save_image(Image.fromarray(dithered_image), output_path)
+
+        input_path = './images/hatsune_miku.png'
+        output_path = './images/hatsune_miku_green_noise_dithered_2_bits.png'
+        image = self.load_image(input_path)
+        dithered_image = self.get_dithered_image(
+            image, 2, lau_arce_gallagher, hysteresis_constant=1/2.5)
+        self.save_image(Image.fromarray(dithered_image), output_path)
 
 
 if __name__ == '__main__':
